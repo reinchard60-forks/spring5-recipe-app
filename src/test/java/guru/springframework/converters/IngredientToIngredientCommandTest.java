54c7bda9a57ac47be37db5f8c41563a0f36533ff
reinchard_lang <reinchard60@hotmail.com>
2020-07-21 09:09:45 -0500

    Recipe Delete

--- src/test/java/guru/springframework/converters/IngredientToIngredientCommandTest.java
+++ src/test/java/guru/springframework/converters/IngredientToIngredientCommandTest.java
@@ -1,7 +1,6 @@
 package guru.springframework.converters;
 
 import guru.springframework.commands.IngredientCommand;
-import guru.springframework.commands.UnitOfMeasureCommand;
 import guru.springframework.domain.Ingredient;
 import guru.springframework.domain.Recipe;
 import guru.springframework.domain.UnitOfMeasure;
@@ -38,26 +37,23 @@ public class IngredientToIngredientCommandTest {
     }
 
     @Test
-    public void convert() throws Exception {
+    public void convertWithNullUOM() throws Exception {
         //given
-        Ingredient command = new Ingredient();
-        command.setId(ID_VALUE);
-        command.setRecipe(RECIPE);
-        command.setAmount(AMOUNT);
-        command.setDescription(DESCRIPTION);
-        UnitOfMeasureCommand unitOfMeasureCommand = new UnitOfMeasureCommand();
-        unitOfMeasureCommand.setId(UOM_ID);
-        command.setUom(null);
+        Ingredient ingredient = new Ingredient();
+        ingredient.setId(ID_VALUE);
+        ingredient.setRecipe(RECIPE);
+        ingredient.setAmount(AMOUNT);
+        ingredient.setDescription(DESCRIPTION);
+        ingredient.setUom(null);
 
         //when
-        IngredientCommand ingredientCommand = converter.convert(command);
+        IngredientCommand ingredientCommand = converter.convert(ingredient);
 
         //then
-        assertNotNull(ingredientCommand);
-        assertNotNull(ingredientCommand.getUom());
+        assertNull(ingredientCommand.getUom());
         assertEquals(ID_VALUE, ingredientCommand.getId());
+        assertEquals(AMOUNT, ingredient.getAmount());
         assertEquals(DESCRIPTION, ingredientCommand.getDescription());
-        assertEquals(UOM_ID, ingredientCommand.getUom().getId());
     }
 
     @Test
@@ -72,12 +68,13 @@ public class IngredientToIngredientCommandTest {
         UnitOfMeasure uom = new UnitOfMeasure();
         uom.setId(UOM_ID);
 
+        ingredient.setUom(uom);
         //when
         IngredientCommand ingredientCommand = converter.convert(ingredient);
 
         //then
         assertNotNull(ingredient);
-        assertNull(ingredient.getUom());
+        assertNotNull(ingredient.getUom());
         assertEquals(ID_VALUE, ingredientCommand.getId());
         assertEquals(UOM_ID, ingredientCommand.getUom().getId());
         assertEquals(AMOUNT, ingredientCommand.getAmount());
--- src/test/java/guru/springframework/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -87,4 +87,13 @@ public class RecipeServiceImplTest {
         verify(recipeRepository, never()).findById(anyLong());
 
     }
+
+    @Test
+    public void testDeleteById() throws Exception {
+
+        Long idToDelete = 2L;
+        recipeService.deleteById(idToDelete);
+
+        verify(recipeRepository, times(1)).deleteById(anyLong());
+    }
 }
